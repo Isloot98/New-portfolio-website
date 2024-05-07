606ee4dadf49efbad585c1e52697f3af134761da
Isa sloot <isasloot@hotmail.com>
2024-05-07 13:13:40 +0100

    portfolio v1.2

--- src/slices/ContentIndex/index.tsx
+++ src/slices/ContentIndex/index.tsx
@@ -1,24 +1,24 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+// import { Content } from "@prismicio/client";
+// import { SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `ContentIndex`.
- */
-export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
+// /**
+//  * Props for `ContentIndex`.
+//  */
+// export type ContentIndexProps = SliceComponentProps<Content.ContentIndexSlice>;
 
-/**
- * Component for "ContentIndex" Slices.
- */
-const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for content_index (variation: {slice.variation})
-      Slices
-    </section>
-  );
-};
+// /**
+//  * Component for "ContentIndex" Slices.
+//  */
+// const ContentIndex = ({ slice }: ContentIndexProps): JSX.Element => {
+//   return (
+//     <section
+//       data-slice-type={slice.slice_type}
+//       data-slice-variation={slice.variation}
+//     >
+//       Placeholder component for content_index (variation: {slice.variation})
+//       Slices
+//     </section>
+//   );
+// };
 
-export default ContentIndex;
+// export default ContentIndex;
