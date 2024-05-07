75c8f35ac76d371a46307d8cda93650c78dac885
Isa sloot <isasloot@hotmail.com>
2024-05-07 13:32:06 +0100

    portfolio v1.2.2

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,7 +25,7 @@ const Header = () => {
     <header
       className={`z-50 md:sticky md:top-4 text-white flex justify-center ${
         scrolled
-          ? "bg-slate-300 text-black transition-all duration-500"
+          ? "bg-black transition-all duration-500"
           : "bg-transparent transition-all duration-500"
       }`}
     >
@@ -35,13 +35,13 @@ const Header = () => {
             <Link
               href="/"
               aria-label="home-page"
-              className="text-lg font-semibold hover:text-slate-500 relative"
+              className="text-lg font-semibold hover:text-slate-100 relative"
               onMouseEnter={() => setHoveredLink("home")}
               onMouseLeave={() => setHoveredLink(null)}
             >
               Home
               <span
-                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-500 ${
+                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-100 ${
                   hoveredLink === "home" ? "scale-x-1" : "scale-x-0"
                 } origin-left transition-transform duration-500 ease-in-out`}
               ></span>
@@ -51,13 +51,13 @@ const Header = () => {
             <Link
               href="/projects"
               aria-label="Projects"
-              className="text-lg font-semibold hover:text-slate-500 relative"
+              className="text-lg font-semibold hover:text-slate-100 relative"
               onMouseEnter={() => setHoveredLink("projects")}
               onMouseLeave={() => setHoveredLink(null)}
             >
               Projects
               <span
-                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-500 ${
+                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-100 ${
                   hoveredLink === "projects" ? "scale-x-1" : "scale-x-0"
                 } origin-left transition-transform duration-500 ease-in-out`}
               ></span>
@@ -67,13 +67,13 @@ const Header = () => {
             <Link
               href="/about"
               aria-label="about"
-              className="text-lg font-semibold hover:text-slate-500 relative"
+              className="text-lg font-semibold hover:text-slate-100 relative"
               onMouseEnter={() => setHoveredLink("about")}
               onMouseLeave={() => setHoveredLink(null)}
             >
               About
               <span
-                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-500 ${
+                className={`absolute bottom-0 left-0 w-full h-0.5 bg-slate-100 ${
                   hoveredLink === "about" ? "scale-x-1" : "scale-x-0"
                 } origin-left transition-transform duration-500 ease-in-out`}
               ></span>
